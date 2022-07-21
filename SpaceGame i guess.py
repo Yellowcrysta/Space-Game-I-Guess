4ab0c57c25def6b4edd4a98d5bd33eee6c42cfc6
Yellowcrysta <109705020+Yellowcrysta@users.noreply.github.com>
2022-07-20 19:11:15 -0500

    Add files via upload

---
+++ SpaceGame i guess.py
@@ -0,0 +1,161 @@
+import pgzrun
+import random
+
+WIDTH = 800
+HEIGHT = 600
+
+ship = Actor('playership1_blue')
+ship.x = 370
+ship.y = 550
+
+laserList = []
+laserSpeed = 10
+
+laserListFoe = []
+laserSpeedFoe = -10
+
+deathTimer = 5
+
+foeList = []
+foeSpeed = 3.5
+
+gem = Actor('gemgreen')
+gem.x = random.randint(20, 780)
+gem.y = 0
+
+life = 10
+score = 0
+game_over = False
+started = False
+
+def on_mouse_move(pos):
+    ship.x = pos[0]
+
+def on_mouse_down():
+    print("Mouse button clicked")
+    laser = Actor("laserblue15")
+    laser.angle += 90
+    laser.x = ship.x
+    laser.y = ship.y
+    laser.dead = 0
+    laserList.append(laser)
+
+def update():
+
+    global score, game_over, life, started
+    #if not started:
+
+    if life <= 0:
+        game_over = True
+    if not game_over:
+        if keyboard.left:
+            ship.x = ship.x - 5
+        if keyboard.right:
+            ship.x = ship.x + 5
+
+        gem.y = gem.y + 4 + score / 5
+        if gem.y > 600:
+ #           life = life - 1
+            resetGem(gem)
+
+        if gem.colliderect(ship):
+            resetGem(gem)
+            score = score + 1
+            spawnFoe()
+
+        for foe in foeList:
+            if foe.dead > deathTimer:
+                foeList.remove(foe)
+            elif foe.dead > 0:
+                foe.dead += 1
+            else:
+                if random.randint(1, 100) == 1:
+                    print("Shot Fired")
+                    laser = Actor("laserred01")
+                    laser.angle += 90
+                    laser.x = foe.x
+                    laser.y = foe.y
+                    laser.dead = 0
+                    laserListFoe.append(laser)
+                foe.x = foe.x + (foe.direction * foeSpeed)
+                if foe.x <= 20:
+                    foe.direction = 1
+                elif foe.x >= 780:
+                    foe.direction = -1
+
+        for laser in laserList:
+            if laser.dead > deathTimer:
+                laserList.remove(laser)
+            elif laser.dead > 0:
+                laser.dead += 1
+            else:
+                laser.y -= laserSpeed
+                if laser.y <= -20:
+                    laserList.remove(laser)
+                for foe in foeList:
+                    if laser.colliderect(foe):
+                        laser.dead = 1
+                        foe.dead = 1
+                        laser.image = "laserblue08"
+                        foe.image = "spinner_hit"
+
+        for laser in laserListFoe:
+            if laser.dead > deathTimer:
+                laserListFoe.remove(laser)
+            elif laser.dead > 0:
+                laser.dead += 1
+            else:
+                laser.y -= laserSpeedFoe
+                if laser.y >= 650:
+                    laserListFoe.remove(laser)
+                if laser.colliderect(ship):
+                    laser.dead = 1
+                    life = life - 1
+                    laser.image = "laserred08"
+
+def draw():
+    screen.fill((80,0,70))
+    if game_over:
+        screen.draw.text('Game Over', (360, 300), color=(255,255,255), fontsize=60)
+        screen.draw.text('Score: ' + str(score), (360, 350), color=(255,255,255), fontsize=60)
+    else:
+        for laser in laserList:
+            laser.draw()
+        for laser in laserListFoe:
+            laser.draw()
+        gem.draw()
+        ship.draw()
+        for foe in foeList:
+            foe.draw()
+        screen.draw.text('Score: ' + str(score), (15,10), color=(255,255,255), fontsize=30)
+        screen.draw.text('Health: ' + str(life), (15,30), color=(255,255,255), fontsize=30)
+
+
+def resetGem(thisGem):
+    color = ''
+    rand = random.randint(0, 3)
+    if rand == 0:
+        color = 'gemblue'
+    elif rand == 1:
+        color = 'gemgreen'
+    elif rand == 2:
+        color = 'gemred'
+    elif rand == 3:
+        color = 'gemyellow'
+
+    thisGem.image = color
+    thisGem.x = random.randint(20, 780)
+    thisGem.y = 0
+
+def spawnFoe():
+    foe = Actor('spinner')
+    foe.x = random.randint(20, 780)
+    foe.y = 35
+    foe.direction = random.randint(0, 1)
+    foe.dead = 0
+    if foe.direction == 0:
+        foe.direction = -1
+    foeList.append(foe)
+
+
+pgzrun.go() # Must be last line
