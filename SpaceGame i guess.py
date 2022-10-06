cc6ad7c1b8e0925b4fa67e7138bfe04baab2c70a
Yellowcrysta <109705020+Yellowcrysta@users.noreply.github.com>
2022-10-05 19:06:49 -0500

    Update SpaceGame i guess.py

--- SpaceGame i guess.py
+++ SpaceGame i guess.py
@@ -1,16 +1,20 @@
+# TODO: SPLIT ASSIST LIST BETWEEN ACTIVE AND INACTIVE
+
 import pgzrun
 import random
 import pygame
 import math
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 2560
+HEIGHT = 1440
 
 
 ship = Actor('playership1_blue')
 ship.x = 370
 ship.y = 550
 shipDefense = None
+
+ufo = Actor('ufoblue')
 
 laserList = []
 laserSpeed = 10
@@ -57,23 +61,26 @@ def update():
     if life <= 0:
         game_over = True
     if not game_over:
-        if keyboard.left:
+        checkForDebug()
+
+        if keyboard.left or keyboard.a:
             ship.x = ship.x - 6
             if ship.x < 50:
                 ship.x = 50
-        if keyboard.right:
+        if keyboard.right or keyboard.d:
             ship.x = ship.x + 6
             if ship.x > WIDTH - 50:
                 ship.x = WIDTH - 50
-        if keyboard.up:
+        if keyboard.up or keyboard.w:
             ship.y = ship.y - 6
             if ship.y < 150:
                 ship.y = 150
-        if keyboard.down:
+        if keyboard.down or keyboard.s:
             ship.y = ship.y + 6
             if ship.y > HEIGHT - 50:
                 ship.y = HEIGHT - 50
-        if random.randint(1, 100) == 1:
+
+        if random.randint(1, 700) == 1:
             spawnAssist()
         if random.randint(1, 40) == 1:
             spawnFoe()
@@ -95,13 +102,15 @@ def update():
                 foe.x = foe.x + (foe.direction * foeSpeed)
                 if foe.x <= 20:
                     foe.direction = 1
-                elif foe.x >= 780:
+                elif foe.x >= 2530:
                     foe.direction = -1
 
         for assist in assistList:
             if assist.active == False:
                 assist.y -= assistListSpeed
-                if assist.colliderect(ship):
+                if assist.y >= 1500:
+                    assistList.remove(assist)
+                elif assist.colliderect(ship):
                     assist.active = True
                     assist.angle = 0
                     assist.image = "ufoblue"
@@ -136,11 +145,12 @@ def update():
                 laser.dead += 1
             else:
                 laser.y -= laserSpeedFoe
-                if laser.y >= 650:
+                if laser.y >= 1500:
                     laserListFoe.remove(laser)
                 for assist in assistList:
-                    if laser.colliderect(assist):
-                        laser.dead
+                    if laser.colliderect(assist) and assist.active == True:
+                        laser.dead = 1
+                        laser.image = "laserred08"
 
                 if laser.colliderect(ship):
                     laser.dead = 1
@@ -169,8 +179,10 @@ def draw():
 
 def spawnAssist():
     assist = Actor('powerupblue_bolt')
-    assist.x = random.randint(20, 780)
+    assist.x = random.randint(20, 2530)
     assist.y = 35
+    if assist.y > 1500:
+        assistList.remove(assist)
     assist.time = 0
     assist.active = False
     assistList.append(assist)
@@ -185,8 +197,6 @@ def spawnFoe():
         foe.direction = -1
     foeList.append(foe)
 
-
-## TODO, use math.radians() to convert from radians to degrees
 def placeSpriteAtAngle(sourceSprite, targetSprite, angle, distance):
     sin = math.sin(math.radians(angle))
     cos = math.cos(math.radians(angle))
@@ -197,7 +207,9 @@ def placeSpriteAtAngle(sourceSprite, targetSprite, angle, distance):
     targetSprite.x = newX
     targetSprite.y = newY
 
-
+def checkForDebug():
+    if keyboard.p:
+        spawnAssist()
 
 
 pgzrun.go() # Must be last line
